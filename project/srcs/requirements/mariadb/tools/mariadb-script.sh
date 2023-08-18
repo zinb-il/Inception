e777402fef231d5e56d73a0264154bb8c2eb433a
zinb <zinb9419@gmail.com>
2023-08-18 14:25:59 -0400

    add new configuration for MariaDB

--- project/srcs/requirements/mariadb/tools/mariadb-script.sh
+++ project/srcs/requirements/mariadb/tools/mariadb-script.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
 if [ ! -d /var/lib/mysql/$MYSQL_NAME ]; then
-  /usr/bin/dumb-init --
-  mysql_install_db
-  /usr/share/mariadb/mysql.server start
-  mysql -e "\
-    CREATE DATABASE IF NOT EXISTS ${MYSQL_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci; \
-    CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'; \
-    GRANT ALL ON ${MYSQL_NAME}.* TO '${MYSQL_USER}'@'%'; \
-    ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; \
-    FLUSH PRIVILEGES; \
-    "
+    /usr/bin/mysql_install_db --user=mysql
+    /usr/bin/mysqld_safe --datadir="/var/lib/mysql"
+    /usr/share/mariadb/mysql.server start
+    mysql -e \	    
+     "CREATE DATABASE IF NOT EXISTS ${MYSQL_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci; \
+     CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'; \
+     GRANT ALL ON ${MYSQL_NAME}.* TO '${MYSQL_USER}'@'%'; \
+     ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; \
+     FLUSH PRIVILEGES;"
+    echo "goood"
+
   mysqladmin --user=root --password=$MYSQL_ROOT_PASSWORD shutdown
 fi
-
-exec "$@"
