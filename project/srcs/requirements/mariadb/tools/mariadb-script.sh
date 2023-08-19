a09043b4643c154c48ad1145ffc614bffdf2911e
zinb-il <zinb9419@gmail.com>
2023-08-19 19:44:34 -0400

    mariadb configuration

--- project/srcs/requirements/mariadb/tools/mariadb-script.sh
+++ project/srcs/requirements/mariadb/tools/mariadb-script.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-if [ ! -d /var/lib/mysql/$MYSQL_NAME ]; then
-    /usr/bin/mysql_install_db --user=mysql
-    /usr/bin/mysqld_safe --datadir="/var/lib/mysql"
-    /usr/share/mariadb/mysql.server start
-    mysql -e \	    
-     "CREATE DATABASE IF NOT EXISTS ${MYSQL_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci; \
-     CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'; \
-     GRANT ALL ON ${MYSQL_NAME}.* TO '${MYSQL_USER}'@'%'; \
-     ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; \
-     FLUSH PRIVILEGES;"
-    echo "goood"
-
-  mysqladmin --user=root --password=$MYSQL_ROOT_PASSWORD shutdown
+/usr/bin/mysql_install_db --user=mysql --datadir="/var/lib/mysql"
+if [ ! -d /var/lib/mysql/${MYSQL_NAME} ]; then
+	/usr/share/mariadb/mysql.server start
+	mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+	mysql -e" CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+        mysql -e" GRANT ALL ON ${MYSQL_NAME}.* TO '${MYSQL_USER}'@'%';"
+	#mysql -e" ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+	mysql -e" FLUSH PRIVILEGES;"
+	mysqladmin --user=root --password=$MYSQL_ROOT_PASSWORD shutdown
 fi
+/usr/bin/mysqld_safe
